042d88999fee26f6f9505dcc8362ae777e5a9176
Dmitriy3462 <laimzcash@gmail.com>
2023-05-08 22:35:24 +0300

    Create New Class (User,Category,Product)

---
+++ Storage/DAL/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.DAL.Models
+{
+    internal class Category
+    {
+    }
+}
